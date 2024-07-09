88795b02af63070e22b926dd30415060ce1742a5
Diaa-eldin-Ahmed <diaa.aboelela@gmail.com>
2024-07-09 15:05:00 +0300

    roman commit

--- main.js
+++ main.js
@@ -228,5 +228,7 @@ var romanToInt = function(s) {
       }
   }
   return result;
-
 };
+
+// time complexity is O(n), where n is the length of the input Roman numeral string
+//  space complexity is O(1), as no extra space is required beyond the minimal variables used in the algorithm
